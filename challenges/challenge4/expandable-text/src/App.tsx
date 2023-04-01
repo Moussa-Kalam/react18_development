8287ff49d2024e63b55cf305e98328b031d7d736
Moussa-Kalam <m.amzat@alustudent.com>
2023-04-01 23:56:35 +0200

    Challenge 4 completed
    Building an ExpandableText component

---
+++ challenges/challenge4/expandable-text/src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ExpandableText from "./components/ExpandableText";
+
+const App = () => {
+  return (
+    <ExpandableText>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi
+      accusantium aspernatur, vitae officia sint officiis, rerum laborum
+      molestiae ratione sunt placeat? Eveniet, quasi? Nostrum quo nobis ipsum
+      ullam et pariatur optio cum dolor unde. Doloribus modi dicta veniam illum
+      nobis quas beatae praesentium? Alias, dicta nostrum corporis eos corrupti
+      ipsum asperiores expedita itaque neque placeat tenetur vero sit est nobis
+      explicabo aperiam quo quaerat libero omnis? Delectus minus, dicta
+      doloremque voluptas dolor ea ut quos, non aut accusantium ex placeat
+      commodi esse, itaque natus nihil illum quae soluta. Incidunt corrupti
+      animi voluptatibus similique velit voluptatum dolores molestiae ipsa sequi
+      laborum.
+    </ExpandableText>
+  );
+};
+
+export default App;
