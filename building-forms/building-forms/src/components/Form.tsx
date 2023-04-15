2aef16aaf53b491727f26c2ef6969f998861f0fb
Moussa-Kalam <m.amzat@alustudent.com>
2023-04-15 14:25:46 +0200

    feat: Access input fields with useRef Hook

--- building-forms/building-forms/src/components/Form.tsx
+++ building-forms/building-forms/src/components/Form.tsx
@@ -1,9 +1,21 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useRef } from "react";
 
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const dobRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  let person = { name: "", age: 0, DateOfBirth: new Date(), email: "" };
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("Submitted!");
+    if (nameRef.current !== null) person.name = nameRef.current.value;
+    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+    if (dobRef.current !== null)
+      person.DateOfBirth = new Date(dobRef.current.value);
+    if (emailRef.current !== null) person.email = emailRef.current.value;
+
+    console.log(person);
   };
 
   return (
@@ -12,28 +24,33 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={nameRef} id="name" type="text" className="form-control" />
       </div>
 
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
       </div>
 
       <div className="mb-3">
         <label htmlFor="DOB" className="form-label">
           Date Of Birth
         </label>
-        <input id="DOB" type="date" className="form-control" />
+        <input ref={dobRef} id="DOB" type="date" className="form-control" />
       </div>
 
       <div className="mb-3">
         <label htmlFor="email" className="form-label">
           Email
         </label>
-        <input id="email" type="email" className="form-control" />
+        <input
+          ref={emailRef}
+          id="email"
+          type="email"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
