95b262eced4bfdd3481e91e519acc09af75cf3c1
Moussa-Kalam <m.amzat@alustudent.com>
2023-04-30 12:14:02 +0200

    feat: Handle form validation with React Hook Form and Zod

--- challenges/challenge5/Expense-Tracker/src/App.tsx
+++ challenges/challenge5/Expense-Tracker/src/App.tsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseFilter from "./components/ExpenseFilter";
 import ExpenseForm from "./components/ExpenseForm";
-
-export const categories = ["Groceries", "Utilities", "Entertainment"];
+import categories from "./categories";
 
 function App() {
   const [selectedCategory, setSelectCategory] = useState("");
